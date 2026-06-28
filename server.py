89264f60bed86891a0290139facba5e6085054c6
nevajnoje777 <nevajnoje777@gmail.com>
2026-06-28 16:19:36 +0300

    Update server.py

--- server.py
+++ server.py
@@ -139,4 +139,5 @@ def delete_msg():
 def index(): return send_file("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
